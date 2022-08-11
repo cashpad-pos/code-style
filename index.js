ef439d3d13264f5cca9e0b1d7acc5ebd629b50fa
darii petru <darii.petru@gmail.com>
2022-08-11 11:50:36 +0300

    add no-trailing-space

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ module.exports = {
     ],
     "import/prefer-default-export": "off",
     "no-underscore-dangle": "off",
+    "no-trailing-spaces": "error",
     "object-curly-newline": [
       "error",
       {
