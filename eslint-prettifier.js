b9c495dba2911041571dc4e90f2c51ae6e037e4f
TabanschiAlex <talex.alexandr@gmail.com>
2023-07-14 17:56:12 +0300

    update rules

--- eslint-prettifier.js
+++ eslint-prettifier.js
@@ -15,14 +15,16 @@ module.exports = {
   "plugins": [
     "@typescript-eslint",
     "sonarjs",
-    "import"
+    "import",
+    "prettier"
   ],
   "extends": [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "airbnb-typescript/base",
-    "plugin:sonarjs/recommended"
+    "plugin:sonarjs/recommended",
+    "prettier"
   ],
   "rules": {
     "import/order": ["warn", {
@@ -39,27 +41,6 @@ module.exports = {
     "object-curly-newline": ["error", { "multiline": true, "consistent": true }],
     "eol-last": "error",
     "max-len": ["error", { "code": 120, "ignoreComments": true }],
-    "space-before-blocks": "off",
-    "@typescript-eslint/space-before-blocks": "error",
-    "space-before-function-paren": "off",
-    "@typescript-eslint/space-before-function-paren": ["error", {
-      "anonymous": "always",
-      "named": "never",
-      "asyncArrow": "always"
-    }],
-    "no-multi-spaces": ["error", { "exceptions": { "Property": true }  }],
-    "space-in-parens": ["error", "never"],
-    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1, "maxBOF": 1 }],
-    "block-spacing": "off",
-    "@typescript-eslint/block-spacing": "error",
-    "array-bracket-spacing": ["error", "always", { "objectsInArrays": false, "arraysInArrays": false }],
-    "newline-before-return": "error",
-    "no-var": "error",
-    "@typescript-eslint/explicit-member-accessibility": ["error", {
-      "accessibility": "explicit",
-      "overrides": {
-        "constructors": "no-public"
-      }
-    }]
+    "prettier/prettier": 2
   }
 }
